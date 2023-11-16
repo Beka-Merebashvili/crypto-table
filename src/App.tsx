73571ecbe088a1e8f2e3d09eb5e52c0057a59f16
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-11-16 14:23:27 +0400

    add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
-
+import Input from "./components/Input";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>test</div>;
+  const [amount, setAmount] = useState("");
+  return (
+    <main className="max-w 2xl mx-auto px-4 py-8">
+      <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">
+        Find cheapest BTC
+      </h1>
+      <div className="flex justify-center mt-8">
+        <Input value={amount} onChange={(e) => setAmount(e.target.value)} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
