6460009f4a4f70ad591a2dbc46b187bbdbae8dad
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-11-18 20:51:53 +0400

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import AmountInput from "./components/AmountInput";
 import ResultRow from "./components/ResultRow";
 
 function App() {
   const [amount, setAmount] = useState("100");
+  const [cachedResults, setCschedResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.get("https://rds54favbg.us.aircode.run/cachedValues").then((res) => {
+      setCschedResults(res.data);
+    });
+  }, []);
   return (
     <main className="max-w 2xl mx-auto px-4 py-8">
       <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">
         Find cheapest BTC
       </h1>
       <div className="flex justify-center mt-8">
-        <AmountInput value={amount} onChange={e => setAmount(e.target.value)} />
+        <AmountInput
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </div>
       <div className="mt-6">
-        <ResultRow />
-        <ResultRow />
-        <ResultRow />
-        <ResultRow />
+        {loading && (
+          <>
+            <ResultRow loading={true} />
+            <ResultRow loading={true} />
+            <ResultRow loading={true} />
+            <ResultRow loading={true} />
+          </>
+        )}
       </div>
     </main>
   );
